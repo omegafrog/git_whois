1920450e25b6d14df51474d2349de8197abbeb0a
omegafrog <jiwoo22110103@gmail.com>
2025-03-27 16:54:24 +0900

    init: first init commit

---
+++ src/main/java/org/omegafrog/git_whois/GitWhoisApplication.java
@@ -0,0 +1,13 @@
+package org.omegafrog.git_whois;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitWhoisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitWhoisApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/omegafrog/git_whois/GitWhoisApplicationTests.java
@@ -0,0 +1,13 @@
+package org.omegafrog.git_whois;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitWhoisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
