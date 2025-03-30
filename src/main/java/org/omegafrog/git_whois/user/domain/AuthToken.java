83968fa2b4a0ebe63b22343e6d823ea6fc15aa13
omegafrog <jiwoo22110103@gmail.com>
2025-03-30 12:17:34 +0900

    feat: user entity의 value로 AuthToken 추가

--- src/main/java/org/omegafrog/git_whois/user/domain/AuthToken.java
+++ src/main/java/org/omegafrog/git_whois/user/domain/AuthToken.java
@@ -1,5 +1,19 @@
 package org.omegafrog.git_whois.user.domain;
 
 public class AuthToken {
-	private String accessToken;
+	private final String accessToken;
+	private final String refreshToken;
+
+	public AuthToken(String accessToken, String refreshToken) {
+		this.accessToken = accessToken;
+		this.refreshToken = refreshToken;
+	}
+
+	public String getAccessToken() {
+		return accessToken;
+	}
+
+	public String getRefreshToken() {
+		return refreshToken;
+	}
 }
