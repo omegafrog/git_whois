e84b0e5030b8174a48c259a4cc97679e6926a0f4
omegafrog <jiwoo22110103@gmail.com>
2025-03-30 15:39:30 +0900

    refactor: 기존 GithubOAuth provider 코드 분리

--- src/main/java/org/omegafrog/git_whois/user/application/UserAuthService.java
+++ src/main/java/org/omegafrog/git_whois/user/application/UserAuthService.java
@@ -3,28 +3,19 @@ package org.omegafrog.git_whois.user.application;
 import org.omegafrog.git_whois.user.domain.AuthToken;
 import org.omegafrog.git_whois.user.domain.GithubAccessToken;
 import org.omegafrog.git_whois.user.domain.GithubId;
+import org.omegafrog.git_whois.user.domain.GithubUserInformation;
+import org.omegafrog.git_whois.user.domain.OAuthProvider;
 import org.omegafrog.git_whois.user.domain.User;
-import org.omegafrog.git_whois.user.domain.UserInformation;
 import org.omegafrog.git_whois.user.domain.UserRepository;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestClient;
-
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.ObjectMapper;
 
 @Service
 public class UserAuthService {
 
-	private final ObjectMapper objectMapper;
 	private final UserRepository userRepository;
+	private final OAuthProvider oauthProvider;
 
-	@Value("${spring.security.oauth2.client.registration.github.client-id}")
-	private String clientId;
-
-	@Value("${spring.security.oauth2.client.registration.github.client-secret}")
-	private String clientSecret;
 
 	@Value("${custom.jwt.secret}")
 	private String secret;
@@ -35,32 +26,16 @@ public class UserAuthService {
 	@Value("${custom.jwt.refresh-exp}")
 	private int refreshExp;
 
-
-	public UserAuthService(ObjectMapper objectMapper, UserRepository userRepository) {
-		this.objectMapper = objectMapper;
+	public UserAuthService(UserRepository userRepository, OAuthProvider oauthProvider) {
 		this.userRepository = userRepository;
-	}
-	private RestClient restClient = RestClient.create();
-
-	private GithubAccessToken requestAccessToken(String code) throws JsonProcessingException {
-		// TODO : 외부 API 호출 분리 필요
-		String response = restClient
-			.post()
-			.uri("https://github.com/login/oauth/access_token?client_id=%s&client_secret=%s&code=%s&redirect_url=%s"
-				.formatted(clientId, clientSecret, code, "http://localhost:8080/login/oauth2/code/github"))
-			.header("Accept", "application/json")
-			.retrieve()
-			.body(String.class);
-
-		String accessToken = objectMapper.readTree(response).get("access_token").asText();
-		String tokenType = "Bearer";
-
-		return new GithubAccessToken(accessToken, tokenType);
+		this.oauthProvider = oauthProvider;
 	}
 
-	public AuthToken registerOrLogin(String code) throws JsonProcessingException {
-		GithubAccessToken githubAccessToken = requestAccessToken(code);
-		UserInformation userInfo = getUserInfo(githubAccessToken);
+	public AuthToken registerOrLogin(String code) {
+
+		GithubAccessToken githubAccessToken = (GithubAccessToken)oauthProvider.requestAccessToken(code);
+
+		GithubUserInformation userInfo = (GithubUserInformation)oauthProvider.getUserInformation(githubAccessToken);
 		GithubId id = userInfo.getGithubId();
 
 		User user;
@@ -76,29 +51,6 @@ public class UserAuthService {
 		return user.generateTokens(exp, refreshExp, secret);
 	}
 
-
-	private UserInformation getUserInfo(GithubAccessToken githubAccessToken) throws JsonProcessingException {
-		// TODO : 외부 API 호출 분리 필요
-		String body = restClient
-			.get()
-			.uri("https://api.github.com/user")
-			.header("Authorization", "%s %s"
-				.formatted(githubAccessToken.getTokenType(), githubAccessToken.getAccessToken()))
-			.retrieve()
-			.body(String.class);
-
-		// TODO : json parsing 분리 필요
-		JsonNode jsonNode = objectMapper.readTree(body);
-		String loginName = jsonNode
-			.get("login").asText();
-		String email = jsonNode.get("email").asText();
-		String avatarUrl = jsonNode.get("avatar_url").asText();
-		String nodeId = jsonNode.get("node_id").asText();
-		String name = jsonNode.get("name").asText();
-		GithubId githubId = new GithubId(jsonNode.get("id").asLong());
-
-		return new UserInformation(githubId, loginName, email, avatarUrl, nodeId, name);
-	}
 
 	private boolean canRegister(GithubId githubId, UserRepository userRepository) {
 		return !userRepository.existByGithubId(githubId);
