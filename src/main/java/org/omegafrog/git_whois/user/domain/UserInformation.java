bbbb0e926ee84acb40e08a331881b929adb62c91
omegafrog <jiwoo22110103@gmail.com>
2025-03-30 11:58:46 +0900

    refactor: UserInformation id -> githubId 수정

--- src/main/java/org/omegafrog/git_whois/user/domain/UserInformation.java
+++ src/main/java/org/omegafrog/git_whois/user/domain/UserInformation.java
@@ -1,19 +1,24 @@
 package org.omegafrog.git_whois.user.domain;
 
 public class UserInformation {
+	private final Long githubId;
 	private final String loginName;
 	private final String email;
 	private final String avatarUrl;
 	private final String nodeId;
 	private final String name;
 
-
-	public UserInformation(String loginName, String email, String avatarUrl, String nodeId, String name) {
+	public UserInformation(Long githubId, String loginName, String email, String avatarUrl, String nodeId, String name) {
+		this.githubId = githubId;
 		this.loginName = loginName;
 		this.email = email;
 		this.avatarUrl = avatarUrl;
 		this.nodeId = nodeId;
 		this.name = name;
+	}
+
+	public Long getGithubId() {
+		return githubId;
 	}
 
 	public String getLoginName() {
